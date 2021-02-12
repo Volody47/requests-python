286ff16709f23b5789131856709aa418b7b52e07
Volody47 <volody.sharapov@yandex.ru>
2021-02-11 22:41:16 -0600

    added new test response_header'Content-Type'=='application_json'

--- tests/test_exercises_01.py
+++ tests/test_exercises_01.py
@@ -4,6 +4,13 @@ import requests
 # Exercise 1.1
 # Perform a GET request to http://api.zippopotam.us/us/90210
 # Check that the response status code equals 200
-def test_get_user_with_id_1_check_status_code_equals_200():
+def test_check_status_code_equals_200():
     response = requests.get("http://api.zippopotam.us/us/90210")
     assert response.status_code == 200
+
+# Exercise 1.2
+# Perform a GET request to http://api.zippopotam.us/us/90210
+# Check that the value of the response header 'Content-Type' equals 'application/json'
+def test_check_response_header_equals_application_json():
+    response = requests.get("http://api.zippopotam.us/us/90210")
+    assert response.headers['Content-Type'] == "application/json"
